3dd4db7503290fd2422c48413ec14de0ff933ece
itsbalexy <abrayan.claye@gmail.com>
2022-01-30 21:38:28 -0500

    first commit

---
+++ useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
+const useFetch = (url) => {
+    const isMounted = useRef(true);
+    const [state, setState] = useState({data: null, loading: true, error: null});
+    useEffect(() => {
+        return () => {
+            isMounted.current = false
+        }
+    }, [])
+    useEffect(() => {
+        setState({data: null, loading: true, error: null})
+      fetch(url)
+      .then(answer => answer.json())
+      .then(data => {
+        if(isMounted.current){
+            setState({
+                data,
+                loading: false,
+                error: null
+            })
+        }
+      })
+    }, [url]);
+    return state;
+};
+
+export default useFetch;
